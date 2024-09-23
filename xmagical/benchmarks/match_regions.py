8125731c3ae2875addb7ffa392a1238c2803fabc
Andrea Protopapa <protopapa.andrea@libero.it>
2024-09-23 23:45:46 +0200

    fix number of debris (not randomized)

--- xmagical/benchmarks/match_regions.py
+++ xmagical/benchmarks/match_regions.py
@@ -27,7 +27,7 @@ class MatchRegionsEnv(BaseEnvXirl):
                 observation space.
             use_dense_reward: Whether to use a dense reward or a sparse one.
             rand_layout_full: Whether to randomize the poses of the debris and goal position.
-            rand_shapes: Whether to randomize the shapes and the number of the debris.
+            rand_shapes: Whether to randomize the shapes (and the number) of the debris.
             rand_colors: Whether to randomize the colors of the debris and the
                 goal zone.
     """
@@ -56,7 +56,7 @@ class MatchRegionsEnv(BaseEnvXirl):
         # self.rand_layout_full = rand_layout_full  EDIT!
         self.rand_target_colour = rand_colors
         self.rand_shape_type = rand_shapes
-        self.rand_shape_count = rand_shapes
+        self.rand_shape_count = False
         self.rand_layout_minor = False
         self.rand_layout_full = rand_layout_full
         if rand_shapes:
@@ -77,7 +77,7 @@ class MatchRegionsEnv(BaseEnvXirl):
         if self.use_state:
             # Redefine the observation space if we are using states as opposed
             # to pixels.
-            max_num_debris = 2 + 2 + 2 + 2
+            max_num_debris = 2 + 3
             c = 4 if self.action_dim == 2 else 5
             self.observation_space = spaces.Box(
                 np.array([-1] * (c + 4 * max_num_debris), dtype=np.float32),
